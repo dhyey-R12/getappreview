cd72715c492b6c6400d45fba10fa4e28ea272814
dhyey-R12 <rajparadhyey12@gmail.com>
2022-12-15 22:41:39 +0530

    design complete

--- js/custom.js
+++ js/custom.js
@@ -1,36 +1,6 @@
 // on page scroll animations js
 $(window).on('load',function() {
 	$('.loader-wrapper').fadeOut('slow');
-	$(function() {
-		var observer = new IntersectionObserver(function(entries) {
-			entries.forEach(function(e) {
-				if (!e.isIntersecting) return;
-				e.target.classList.add('move'); // 交差した時の処理
-				observer.unobserve(e.target);
-				// target element:
-				//   e.target				ターゲット
-				//   e.isIntersecting		交差しているかどうか
-				//   e.intersectionRatio	交差している領域の割合
-				//   e.intersectionRect		交差領域のgetBoundingClientRect()
-				//   e.boundingClientRect	ターゲットのgetBoundingClientRect()
-				//   e.rootBounds			ルートのgetBoundingClientRect()
-				//   e.time					変更が起こったタイムスタンプ
-			})
-		},{
-			// オプション設定
-			rootMargin: '0px 0px -5% 0px' //下端から5%入ったところで発火
-			//threshold: [0, 0.5, 1.0]
-		});
-		var target = document.querySelectorAll('.io'); //監視したい要素をNodeListで取得
-		for(var i = 0; i < target.length; i++ ) {
-			observer.observe(target[i]); // 要素の監視を開始
-		}
-		//アニメーションによる各要素のはみ出しを解消
-		$("body").wrapInner("<div style='overflow:hidden;'></div>");
-		// $("#id_selectbox").on("change", function() {
-		// 	$(this).removeClass("holder_col").addClass("active_col");
-		// });
-	});
 });
 
 $(document).ready(function(){
@@ -43,8 +13,54 @@ $(document).ready(function(){
 		$(this).next("nav").toggleClass('navbar_animate');
 	});
 
+	// plan box slider js
+	var $slider = $('.plan_box_slider');
+	var $progressBar = $('.progress');
+	var $progressBarLabel = $( '.slider__label' );
+
+	$slider.on('afterChange', function(event, slick, currentSlide, nextSlide) {   
+		var currentDot = $(".slick-dots .slick-active").index() + 1;
+		var dots = $slider.find('.slick-dots li').length;
+
+
+		var calc = (currentDot/dots) * 100;
+
+		$progressBar
+		.css('background-size', calc + '% 100%')
+		.attr('aria-valuenow', calc );
+
+		$progressBarLabel.text( calc + '% completed' );
+	});
+
+	$slider.slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		speed: 400,
+		dots: true,
+		infinite: false,
+		arrows: false,
+		responsive: [
+		{
+			breakpoint: 1200,
+			settings: {
+				slidesToShow: 2,
+				autoplay: true,
+				autoplaySpeed: 2000,
+			}
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 1,
+				autoplay: true,
+				autoplaySpeed: 2000,
+			}
+		}
+		]
+	});
+
 	// got to page top js
-	$(window).on('load scroll',function(){
+	/*$(window).on('load scroll',function(){
 		var	windowTop = $(window).scrollTop();
 		if(windowTop > 600) {
 			$('.pagetop').fadeIn();
@@ -63,5 +79,5 @@ $(document).ready(function(){
 		$('body,html').animate({
 			scrollTop: 0,
 		}, 800);
-	});
+	});*/
 });
